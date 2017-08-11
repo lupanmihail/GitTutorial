93e72ad71c68747a6fee99eec58f17231b383eb3
Mihail <winterk1000@yahoo.com>
2017-08-11 16:20:54 +0300

    Start solving problems

--- Untitled1.c
+++ Untitled1.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 void main(void){
-puts("Hello World");
+int i,a[4];
+for (i=0,i<4,i++){
+scanf("%s",&a[i]);
+}
+for (i=3,i<=0,i--){
+printf("%d", sqrt(a[i]);
+]
 }
